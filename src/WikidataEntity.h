97444214c45898c6b9b2dbcc8177e2c4f7ac087e
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2018-10-08 17:45:37 +0200

    Start refactoring of WikidataEntity

--- src/WikidataEntity.h
+++ src/WikidataEntity.h
@@ -1,10 +1,7 @@
 #ifndef _WIKIDATA_ENTITY_H
 #define _WIKIDATA_ENTITY_H
 
-#include <cereal/archives/json.hpp>
-#include <cereal/cereal.hpp>
 #include <iostream>
-#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -12,33 +9,35 @@
 using std::string;
 using json = nlohmann::json;
 
-// _________________________________________________________________________-
+// Wikidata has items/subjects (Q42) and Properties(P31)
 enum class EntityType {
   Subject, Property};
 
+// Simple Conversion function
 inline std::string entityTypeToString(EntityType t) {
   return t == EntityType::Subject ? "Q" : "P";
 }
 
-// ________________________________________________________________
+// Different Aspects after which we can sort entities
 enum class OrderType {
   None, NumSitelinks, Numeric, Alphabetical
 };
 
 // Converter Function
-std::string EntityTypeToString(const EntityType& type); 
+// TODO: Rename/Refactor this
+std::string EntityTypeToString(const EntityType& type);
 
-// a simple class handling metadata of a wikidata entity
+// Class for the metadata of a wikidata entity
 class WikidataEntity {
  public:
   string name;
   string description;
   std::vector<string> aliases;
   unsigned int numSitelinks;
-    
-  
-  // read line that python preprocessor outputs
-  // TODO: document format when finished
+
+  // Read from line of the entity/alias File
+  // (see README). Is not able to parse the description
+  // which has to be set manually
   WikidataEntity(const string& line);
 
 
@@ -53,7 +52,9 @@ class WikidataEntity {
   }
 };
 
-// for search results, only index, name and description
+// A smaller class for the meta data of a Wikidata entitye
+// Used for results of searches and queries
+// Does not need the aliases
 class WikidataEntityShort {
  public:
   string wdName;
@@ -62,6 +63,7 @@ class WikidataEntityShort {
   int numSitelinks;
   EntityType type;
 
+  // Constructor
   WikidataEntityShort(const string& wd, const string& nameT, const string& desc, unsigned int nSitelinks = 0)
     : wdName(wd), name(nameT), description(desc), numSitelinks(nSitelinks) {
     type = WikidataEntity::IsPropertyName(wdName) ? EntityType::Property : EntityType::Subject;}
@@ -69,10 +71,12 @@ class WikidataEntityShort {
   // default constructor needed for resize etc
   WikidataEntityShort() = default;
 
+  // TODO: not needed with new JSON library
   static json
   nestedVecToArray(const std::vector<std::vector<WikidataEntityShort>> &vec);
 
   // _____________________________________________________________________________
+  // TODO: we are probably not doing the sorting here but in QLever
   static void sortVec(std::vector<std::vector<WikidataEntityShort>>& vec, size_t orderVarIdx, OrderType type, bool asc);
 
   string toString() {return wdName + "\t" + name + "\t" + description;}
